434d0f0856f1f6ce7acb73bd8d16ca814a244bc3
choi ji hun <chl020729@gmail.com>
2023-12-26 16:33:22 +0900

    K Solution Upload

---
+++ solutions/K. 게임 개발자 상상부기.cpp
@@ -0,0 +1,172 @@
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0),cin.tie(0)
+#define endl '\n'
+using namespace std;
+class Room {
+private:
+    string title;
+    vector<int>users;
+    int adminId;
+public:
+    Room(string& title, int& id) {
+        this->title = title;
+        this->adminId = id;
+    }
+    int getAdminId() {
+        return adminId;
+    }
+    void setAdminId(int& adminId) {
+        this->adminId = adminId;
+    }
+    string getTitle() {
+        return title;
+    }
+    vector<int>& getUsers() {
+        return users;
+    }
+    void setTitle(string& title) {
+        this->title = title;
+    }
+    void setUsers(vector<int>& users) {
+        this->users = users;
+    }
+};
+class Manage {
+public:
+    vector<Room>data;
+    Manage() {
+        data.clear();
+    }
+    bool enterRoom(string& title, int& id) {
+        //어느 유저가 어느 방으로 들어가는지
+        bool ret = false;
+        for (Room& r : data) {
+            if (r.getTitle() == title) {
+                r.getUsers().push_back(id);
+                ret = true;
+                break;
+            }
+        }
+        return ret;
+    }
+    int isAlreadyMake(string& title) {//이미 만들어진 적이 있는지 적발
+        int index = -1;
+        for (int i = 0; i < data.size(); i++) {
+            if (data[i].getTitle()==title) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    bool gameStart(string& title, int& id) {//어떤 아이가 게임을 시작했는지 중요함
+        bool ret = false;
+        for (Room r : data) {
+            if (r.getTitle() == title) {
+                if (r.getAdminId() == id) {
+                    ret = true;
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    bool makeRoom(string& title, int& id) {
+        int index = isAlreadyMake(title);//이미 만들어진 적이 있는지 확인
+        if (index != -1) {//이미 만들어진 적이 있으면 방장만 바꾸자.
+            this -> data[index].setAdminId(id);
+            this -> data[index].getUsers().push_back(id);
+            return false;
+        }
+        Room newRoom(title, id);
+        newRoom.getUsers().push_back(id);
+        data.push_back(newRoom);
+        return true;
+    }
+    bool gameEnd(string& title, int& id) {
+        bool ret = false;
+        vector<Room>::iterator it = data.begin();
+        while (it != data.end()) {
+            if ((*it).getTitle() == title) {
+                if ((*it).getAdminId() == id) {
+                    ret = true;
+                }
+                data.erase(it);
+                break;
+            }
+            it++;
+        }
+        return ret;
+    }
+};
+
+class Error {
+private:
+    string time;
+    int id;
+public:
+    Error(string& time, int& id) {
+        this->time = time;
+        this->id = id;
+    }
+    int getId() {
+        return id;
+    }
+    string getTime() {
+        return time;
+    }
+    void setId(int& id) {
+        this->id = id;
+    }
+    void setTime(string& time) {
+        this->time = time;
+    }
+};
+void errorAdd(vector<Error>&errors, Error newError, bool Already[]) {
+    int id = newError.getId();
+    if (Already[id])return;
+    errors.push_back(newError);
+    Already[id] = true;
+}
+int main() {
+	fast;
+    vector<Error>errors;
+    bool Numbering[10002]{};//숫자 이미 출력했는지 중복체크
+    Manage manage;
+    int n; cin >> n;
+    string title = "";
+    for (int i = 0; i < n; i++) {
+        string time, mode;
+        int id;
+        cin >> time >> id >> mode;
+        if (mode[0] == 'm') {
+            cin >> title;
+            bool flag = manage.makeRoom(title, id);
+            if (!flag)errorAdd(errors, Error(time, id), Numbering);
+        }
+        else if (mode[0] == 'i') {
+            cin >> title;
+            manage.enterRoom(title, id);
+        }
+        else if (mode[0] == 's') {
+            cin >> title;
+            bool flag = manage.gameStart(title, id);
+            if (flag == 0) {//id 안 맞는 아이가 시작함.
+                errorAdd(errors, Error(time, id), Numbering);
+            }
+        }
+        else if (mode[0] == 'e') {
+            cin >> title;
+            bool flag = manage.gameEnd(title, id);
+            if (!flag)errorAdd(errors, Error(time, id), Numbering);
+        }
+    }
+    for (Error& output : errors) {
+        cout << output.getTime() << " " << output.getId() << endl;
+    }
+    return 0;
+}
---
+++ solutions/K. 게임 개발자 상상부기.java
@@ -0,0 +1,176 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+/*
+ * 
+ *
+ */
+public class Main {
+    static class ManageRoom{
+        List<Room>data;
+        public ManageRoom(){
+            data=new ArrayList<>();
+        }
+        public boolean enterRoom(String title,int id){
+            //어느 유저가 어느 방으로 들어가는지
+            boolean flag=false;
+            for(int i=0;i<data.size();i++){
+                if(data.get(i).title.equals(title)){//내가 들어갈 방과 이름이 같다면
+                    flag= data.get(i).users.add(id);
+                    break;
+                }
+            }
+            return flag;
+        }
+        public int isAlreadyMake(String title){//이미 만들어진 적이 있는지 적발
+            int index = -1;
+            for(int i=0;i<data.size();i++){
+                if(data.get(i).title.equals(title)){
+                    index=i;
+                    break;
+                }
+            }
+            return index;
+        }
+        public boolean gameStart(String title,int id){//어떤 아이가 게임을 시작했는지 중요함
+            boolean ret = false;
+            for(Room r:data){
+                if(r.getTitle().equals(title)){
+                    if(r.getAdminId()==id){
+                        ret = true;
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+        public boolean makeRoom(String title,int id){
+            int index = isAlreadyMake(title);//이미 만들어진 적이 있는지 확인
+            if(index!=-1){//이미 만들어진 적이 있으면 방장만 바꾸자.
+                this.data.get(index).setAdminId(id);
+                this.data.get(index).users.add(id);
+                return false;
+            }
+            Room newRoom = new Room(title,id);
+            newRoom.users.add(id);
+            return data.add(newRoom);
+        }
+        public boolean gameEnd(String title, int id) {
+            boolean ret = false;
+            for(int i=0;i<data.size();i++){//10000
+                Room r = data.get(i);
+                if(r.getTitle().equals(title)){
+                    if(r.getAdminId() == id){
+                        ret = true;
+                    }
+                    data.remove(i);//10000번
+                    break;
+                }
+            }
+            return ret;
+        }
+    }
+    static class Room{
+        private String title;
+        private List<Integer>users;
+        private int adminId;
+        public Room(String title,int id){
+            this.users = new ArrayList<>();
+            this.title =title;
+            this.adminId =id;
+        }
+        public int getAdminId() {
+            return adminId;
+        }
+        public void setAdminId(int adminId) {
+            this.adminId = adminId;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public List<Integer> getUsers() {
+            return users;
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        public void setUsers(List<Integer> users) {
+            this.users = users;
+        }
+    }
+    static class Error{
+        private String time;
+        private int id;
+        public Error(String time,int id){
+            this.time = time;
+            this.id = id;
+        }
+        public int getId() {
+            return id;
+        }
+        public String getTime() {
+            return time;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
+    public static void errorAdd(List<Error>errors,Error newError,boolean[] Already){
+        int id = newError.getId();
+        if(Already[id])return;
+        errors.add(newError);
+        Already[id]=true;
+    }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();//결과
+        StringTokenizer log;
+        List<Error>errors = new ArrayList<>();
+        boolean[] Numbering = new boolean[10002];
+
+        ManageRoom mr = new ManageRoom();
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){//n번
+            log = new StringTokenizer(br.readLine());
+            String time = log.nextToken();
+            int id = Integer.parseInt(log.nextToken());
+            String mode = log.nextToken();
+            switch (mode) {
+                case "make"->{
+                    String title = log.nextToken();
+                    boolean flag = mr.makeRoom(title, id);
+                    if(!flag){//만들기 실패
+                        errorAdd(errors, new Error(time,id), Numbering);
+                    }
+                }   
+                case "into"->{
+                    String title = log.nextToken();
+                    mr.enterRoom(title, id);
+                }   
+                case "start"->{
+                    String title = log.nextToken();
+                    boolean flag = mr.gameStart(title, id);
+                    if(!flag){//id 안 맞는 아이가 시작함.
+                        errorAdd(errors, new Error(time,id), Numbering);
+                    }
+                }  
+                case "end"->{
+                    String title = log.nextToken();
+                    boolean flag =mr.gameEnd(title, id);
+                    if(!flag)errorAdd(errors, new Error(time, id), Numbering);
+                }
+            }
+        }
+        for(Error output:errors){
+            sb.append(output.getTime() + " " +output.getId()+"\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
