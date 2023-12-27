23772337e2c2fede5846ae039ea3b0cb44f2dc07
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-12-27 14:31:50 +0900

    Add problem B and solutions

---
+++ solutions/B. 뽀삐뽀삐뽀 뽀삐뽀.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int W, B, M;
+    scanf("%d %d %d", &W, &B, &M);
+
+    int result = W + B - M;
+    printf("%d\n", result);
+
+    return 0;
+}
---
+++ solutions/B. 뽀삐뽀삐뽀 뽀삐뽀.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int W, B, M;
+    cin >> W >> B >> M;
+
+    int result = W + B - M;
+    cout << result << endl;
+
+    return 0;
+}
