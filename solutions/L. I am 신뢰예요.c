368be59d63eca2eb5bcc2c83bf3bd6f1baed2d91
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-05 14:13:35 +0900

    Update L. I am 신뢰예요.c

--- solutions/L. I am 신뢰예요.c
+++ solutions/L. I am 신뢰예요.c
@@ -7,7 +7,7 @@ int compare(void* a,void *b){
     return *A > *B;
 }
 
-int Upper_bound(int target,int size, int array[]){
+int upperBound(int target,int size, int array[]){
     int left=0,right=size-1;
     while(left<=right){
         int mid =(left+right)/2;
@@ -20,15 +20,23 @@ int Upper_bound(int target,int size, int array[]){
     return left;
 }
 int main(){
-    int n,m;scanf("%d %d",&n,&m);
-    for(int i=0;i<n;i++)scanf("%d",&cats[i]);
-    for(int i=0;i<m;i++)scanf("%d",&students[i]);
+    int n,m;
+    scanf("%d %d",&n,&m);
 
+    // 고양이 몸무게 입력받기
+    for(int i=0;i<n;i++)
+        scanf("%d",&cats[i]);
+    // 학생 키 입력받기
+    for(int i=0;i<m;i++)
+        scanf("%d",&students[i]);
+
+    // 정렬
     qsort(students,m,sizeof(int),compare);
 
+    // 모든 고양이들에 대해서 '착한인간구별공식' 적용
     for(int i=0;i<n;i++){
         int target = cats[i];
-        int cnt = Upper_bound(target,m,students);
+        int cnt = upperBound(target,m,students);
         printf("%d ",cnt);
     }
     printf("\n");
