173da65a209c1b9be073a8cc47459b46ce49126b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-05 14:16:51 +0900

    Update L. I am 신뢰예요.cpp

--- solutions/L. I am 신뢰예요.cpp
+++ solutions/L. I am 신뢰예요.cpp
@@ -3,10 +3,9 @@
 #include <vector>
 #define fast ios::sync_with_stdio(0), cin.tie(0)
 #define endl '\n'
-
 using namespace std;
 
-#1번 STL 이용한 풀이
+//1번 STL 이용한 풀이
 int compare(int &a, int &b) { return a < b; }
 
 int main() {
@@ -20,8 +19,9 @@ int main() {
   for (int i = 0; i < m; i++)
     cin >> students[i];
 
-  sort(students.begin(), students.end(), compare); //학생들 오름차순
+  sort(students.begin(), students.end(), compare); // 학생키 오름차순 정렬
 
+  // 모든 고양이들의 키에 대해서 '착한인간구별공식' 을 만족하는 학생의 마지막 인덱스 파악
   for (int i = 0; i < n; i++) { // n
     int cat_height = cats[i];
     int cnt = upper_bound(students.begin(), students.end(), cat_height) - students.begin(); // logm
@@ -30,7 +30,8 @@ int main() {
   cout << endl;
   return 0;
 }
-#2번 이분탐색 구현코드
+
+//2번 이분탐색 구현코드
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -43,7 +44,7 @@ int compare(int &a,int &b){
     return a<b;
 }
 
-int Upper_bound(int target, vector<int>&array){
+int upperBound(int target, vector<int>&array){
     int left= 0,right = array.size()-1;
 
     while(left<=right){//
@@ -65,11 +66,12 @@ int main(){
     for(int i=0;i<n;i++)cin >> cats[i];
     for(int i=0;i<m;i++)cin >> students[i];
 
-    sort(students.begin(),students.end(),compare);//학생들 오름차순
-    //mlogm
+    sort(students.begin(),students.end(),compare); // 학생키 오름차순 정렬
+
+    // 모든 고양이들의 키에 대해서 '착한인간구별공식' 을 만족하는 학생의 마지막 인덱스 파악
     for(int i=0;i<n;i++){//n
         int cat_height = cats[i];
-        int cnt = Upper_bound(cats[i],students); //logm
+        int cnt = upperBound(cats[i],students); //logm
         cout << cnt << ' ';
     }
     cout << endl;
