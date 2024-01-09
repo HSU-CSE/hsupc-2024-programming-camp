a8ae2b33949beea2eae179de730756bcb090e8a2
Striver <96542456+T3Tm@users.noreply.github.com>
2024-01-09 23:59:12 +0900

    C Solution Update

--- solutions/C. 홍 박사님을 아세요.c
+++ solutions/C. 홍 박사님을 아세요.c
@@ -1,28 +1,25 @@
-#include <stdbool.h>
 #include <stdio.h>
-bool compare(int idx, char word[]) {
-  char words[] = "Hong";
-  int i = idx;
-  for (; word[i] && words[i - idx]; i++) {
-    if (word[i] != words[i - idx])
-      return 0;
-  }
-  if (i != idx + 4)
-    return 0;
-  return 1;
-}
+//1번 풀이
 main() {
   char word[102];
   scanf("%s", word);
   int cnt = 0;
   for (int i = 0; word[i]; i++) {
     if (word[i] == 'H') {
-      bool ret = compare(i, word);
-      if (ret) {
-        cnt++;
-        i += 3;
-      }
+      cnt++;
     }
+  }
+  printf("%d", cnt);
+}
+
+//2번 풀이
+
+main() {
+  char word[102];
+  scanf("%s", word);
+  int cnt = 0;
+  for (int i = 0; word[i]; i++) {
+    cnt+=word[i]=='H';
   }
   printf("%d", cnt);
 }
--- solutions/C. 홍 박사님을 아세요.java
+++ solutions/C. 홍 박사님을 아세요.java
@@ -1,29 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-  static boolean compare(String word, int idx) {
-    String words = "Hong";
-    int i = idx;
-    for (; i - idx < words.length() && i < word.length(); i++) {
-      if (words.charAt(i - idx) != word.charAt(i))
-        return false;
-    }
-    if (i != idx + 4)
-      return false;
-    return true;
-  }
-
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     String word = s.next();
     int cnt = 0;
     for (int i = 0; i < word.length(); i++) {
       if (word.charAt(i) == 'H') {
-        boolean ret = compare(word, i);
-        if (ret) {
-          i += 3;
-          cnt++;
-        }
+        cnt++;
       }
     }
     System.out.println(cnt);
