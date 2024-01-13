891ba1f331aaf27b636563ddf2f22f38ad011708
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2024-01-13 23:01:27 +0900

    Update J. 세 정수.cpp

--- solutions/J. 세 정수.cpp
+++ solutions/J. 세 정수.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int solve(int n) {
     int count = 0;
     for (int a = -n; a <= n; ++a) {
-        if (a == 0) continue;
         for (int b = -n; b <= n; ++b) {
             int c = -2 * (a + b);
-            if (abs(c) <= n) count++;
+            if (abs(c) != 0 && abs(c) <= n) count++;
         }
     }
     return count + (2 * n + 1) * (2 * n + 1);
