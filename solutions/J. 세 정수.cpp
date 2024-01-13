d27875f26130faa759cfb49239fb813de2c7554f
Gyeongmin <97784561+gyeongminn@users.noreply.github.com>
2024-01-13 23:12:23 +0900

    Update J. 세 정수.cpp

--- solutions/J. 세 정수.cpp
+++ solutions/J. 세 정수.cpp
@@ -1,8 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int solve(int n) {
+int solve1(int n) {
     int count = 0;
     for (int a = -n; a <= n; ++a) {
         for (int b = -n; b <= n; ++b) {
@@ -13,10 +13,25 @@ int solve(int n) {
     return count + (2 * n + 1) * (2 * n + 1);
 }
 
+int solve2(int n) {
+    int sum = 0;
+    for (int k = 1; k <= n / 2; ++k) {
+        sum += 2 * n + 1 - k;
+    }
+    return 2 * sum + (2 * n + 1) * (2 * n + 1);
+}
+
+int solve3(int n) {
+    int answer = (2 * n + 1) * (2 * n + 1);
+    int tmp = n / 2;
+    answer += 2 * ((tmp * (2 * n + 1)) - (tmp * (tmp + 1) / 2));
+    return answer;
+}
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
     int n;
     cin >> n;
-    cout << solve(n);
+    cout << solve1(n);
     return 0;
 }
