b52b0f50506483f2b6b9fa424b7fceb80c5e228e
junhaa <94986147+junhaa@users.noreply.github.com>
2024-01-14 21:32:38 +0900

    Update G. 통모짜핫도그.java

--- solutions/G. 통모짜핫도그.java
+++ solutions/G. 통모짜핫도그.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
+// G. 통모짜핫도그
 public class Main {
-    static int nextInt(StringTokenizer st) {
-        return Integer.parseInt(st.nextToken());
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-
-        sb.append("YES\n");
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int endPoint = nextInt(st) + nextInt(st);
-        for (int repeat = 0; repeat < n - 1; repeat++) {
-            st = new StringTokenizer(br.readLine());
-            int s = nextInt(st);
-            int b = nextInt(st);
-            if (s >= endPoint) {
-                sb.append("YES");
-            } else {
-                sb.append("NO");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int endTime = -1;
+        int answer = 0;
+        for(int i = 0 ; i < N ; i ++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            if(i == 0){
+                endTime = end;
+                answer ++;
             }
-            endPoint = Math.max(endPoint, s + b);
-            sb.append("\n");
+            else{
+                if(start < endTime);
+                else{
+                    answer ++;
+                    endTime = start + end;
+                }
+            }
         }
-        
-        System.out.println(sb);
+        System.out.println(answer);
     }
 }
